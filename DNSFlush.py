f0fcee4536518597de52345b34cabbabbb912283
Sebastien R Roux <SebastienRRoux1997@gmail.com>
2022-10-28 21:51:53 +0200

    Update DNSFlush.py

--- DNSFlush.py
+++ DNSFlush.py
@@ -1,12 +1,12 @@
 import os
-commands = ['sudo systemd-resolve --flush-caches', 'sudo systemctl restart systemd-resolved']
+commands = ['sudo resolvectl flush-caches', 'sudo systemctl restart systemd-resolved']
 y = True
 while (y == True):
     x = input('Press q to quit, s for statistics or f to flush')
     if (x == 'q'):
         y = False
     elif (x == 's'):
-        os.system('sudo systemd-resolve --statistics')
+        os.system('sudo resolvectl statistics')
     elif (x == 'f'):
         for command in commands:
             os.system(command)
